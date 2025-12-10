8b6f659036271a85b5f6c4d3b9edce4e89e2a356
ionutbirlad <birladionut@yahoo.it>
2025-12-10 17:12:03 +0100

    chore: initial linters setup

---
+++ .eslint.cjs
@@ -0,0 +1,57 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    // deve stare SEMPRE per ultima: spegne le regole di stile che
+    // vanno in conflitto con Prettier
+    'prettier',
+  ],
+  rules: {
+    // Con React 17+ / Vite non serve più importare React a mano
+    'react/react-in-jsx-scope': 'off',
+    // Usi TS, quindi niente PropTypes
+    'react/prop-types': 'off',
+
+    // Meno rumore sugli unused, ma teniamo cose sensate
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+
+    // (Facoltativo) Ordine import migliore
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+};
